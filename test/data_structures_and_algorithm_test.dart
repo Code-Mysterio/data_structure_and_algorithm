4e1876bbbaa68ffc8b8c8108a055bd29f1f236b5
Code_Mysterio <abdulyoung78@gmail.com>
2023-09-20 12:46:56 +0100

    first commit

---
+++ test/data_structures_and_algorithm_test.dart
@@ -0,0 +1,8 @@
+import 'package:data_structures_and_algorithm/data_structures_and_algorithm.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
